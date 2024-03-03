98a3b1a2538bdf0c22fa29a8ac27283800fc43be
myrmanshelby <myrmanshelby@gmail.com>
2024-03-02 22:24:59 -0800

    matched output to spec

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,22 @@
-const sumAll = function() {
-
+const sumAll = function (start, stop) {
+  let sum = 0;
+  if (
+    start < 0 ||
+    stop < 0 ||
+    typeof start != "number" ||
+    typeof stop != "number"
+  ) {
+    return "ERROR";
+  }
+  if (start > stop) {
+    let temp = start;
+    start = stop;
+    stop = temp;
+  }
+  for (let i = start; i <= stop; i++) {
+    sum += i;
+  }
+  return sum;
 };
 
 // Do not edit below this line
