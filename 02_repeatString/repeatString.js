fba8752c0de053c5bbd36b9af27d2149cbe3827b
myrmanshelby <myrmanshelby@gmail.com>
2024-03-02 21:33:57 -0800

    Edited code to past spec tests

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function (repeatedString, timesRepeated) {
+  let returnString = "";
+  if (timesRepeated < 0) {
+    return "ERROR";
+  }
+  for (let i = 0; i < timesRepeated; i++) {
+    returnString += repeatedString;
+  }
+  return returnString;
 };
 
 // Do not edit below this line
