fe82357399464ba1304c979dc7bdeea3c28967d2
myrmanshelby <myrmanshelby@gmail.com>
2024-03-02 22:08:13 -0800

    matched sample output to spec output

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,17 @@
-const removeFromArray = function() {
-
+const removeFromArray = function (compArray, ...removeValues) {
+  let cleanArray = [];
+  for (let i = 0; i < compArray.length; i++) {
+    let keep = true;
+    for (let j = 0; j < removeValues.length; j++) {
+      if (compArray[i] === removeValues[j]) {
+        keep = false;
+      }
+    }
+    if (keep) {
+      cleanArray.push(compArray[i]);
+    }
+  }
+  return cleanArray;
 };
 
 // Do not edit below this line
