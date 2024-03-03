bfacd41a227a82c410cb728dea1bc2c22ca19b87
myrmanshelby <myrmanshelby@gmail.com>
2024-03-02 21:44:06 -0800

    Updated reverseString.js to match spec

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function (regular) {
+  reversed = "";
+  for (let i = regular.length - 1; i >= 0; i--) {
+    reversed += regular[i];
+  }
+  return reversed;
 };
 
 // Do not edit below this line
