38a8bf86c62bf0af1a9578e881c2747768f5f665
myrmanshelby <myrmanshelby@gmail.com>
2024-03-04 14:57:30 -0800

    Added formulas for tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (farenheitTemp) {
+  return Math.round((farenheitTemp - 32) * (5 / 9) * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (celciusTemp) {
+  return Math.round(((9 / 5) * celciusTemp + 32) * 10) / 10;
 };
 
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
-  convertToFahrenheit
+  convertToFahrenheit,
 };
