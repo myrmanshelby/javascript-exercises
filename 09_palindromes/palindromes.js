62181721d9a3424c422dc22bf5d7670fab71dfc5
myrmanshelby <myrmanshelby@gmail.com>
2024-04-21 20:22:21 -0700

    edited files to pass spec tests

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,12 @@
-const palindromes = function () {
+const palindromes = function (word) {
+  const wordArray = word
+    .toLowerCase()
+    .split("")
+    .filter((item) => !"!(),.;:/? ".includes(item));
 
+  const forwardWord = wordArray.join("");
+  const reverseWord = wordArray.reverse().join("");
+  return forwardWord === reverseWord;
 };
 
 // Do not edit below this line
--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,21 @@
-const findTheOldest = function() {
+const findTheOldest = function (arr) {
+  let oldestPerson;
+  let oldestAge = 0;
 
+  for (let i = 0; i < arr.length; i++) {
+    let currentAge;
+    if (!arr[i].yearOfDeath) {
+      currentAge = new Date().getFullYear() - arr[i].yearOfBirth;
+    } else {
+      currentAge = arr[i].yearOfDeath - arr[i].yearOfBirth;
+    }
+
+    if (currentAge > oldestAge) {
+      oldestAge = currentAge;
+      oldestPerson = arr[i];
+    }
+  }
+  return oldestPerson;
 };
 
 // Do not edit below this line
